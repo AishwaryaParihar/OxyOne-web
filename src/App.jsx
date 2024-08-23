0d44e12bfba892a6194560a8880349489840d7de
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-23 12:26:13 +0530

    ADD webview stling

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,18 @@ function App() {
   return (
     <>
  
-   <Outlet/>
+ <div className="flex flex-col justify-center items-center bg-secondary">
+      <div className="lg:w-6/12 xl:w-[470px] sm:w-8/12 w-full min-h-screen border border-5 shadow-md shadow-zinc-300 relative">
+        {/* <Header /> */}
+        <div className="w-full h-full flex flex-col mt-3">
+          {/* {children} */}
+          {/* <Outlet/> */}
+        <h1  className='text-red-300'>aaaaaaaaaaaa</h1>
+          <Outlet/>
+        </div>
+        {/* <Footerbar /> */}
+      </div>
+    </div>
     </>
   )
 }
